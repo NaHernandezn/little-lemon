c8977bd00f9450cce509d75d55e7cc400ee4340c
NaHernandezn <nata992010@hotmail.es>
2024-09-04 16:34:20 -0500

    Changed app.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import Logo from './Assets/Logo.svg'
 import { Helmet } from 'react-helmet';
 import Nav from './Nav';
-import { Route, Routes, BrowserRouter as Router } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import OnlineMenu from './OnlineMenu'
 import Reservations from './Reservations';
 import Footer from './Footer';
@@ -13,28 +13,26 @@ import Homepage from './Homepage'
 
 function App() {
   return (
-    <Router basename='/little-lemon'>
-      <section className='App'>
-        <Helmet>
-          <meta name="og:description" content="Little Lemon is a family-owned restaurant specialized in Mediterranean food with a modern twist"/>
-          <meta name="og:title" content='Little Lemon'/>
-          <meta name='og:image' content={Logo}/>
-          <link rel="preconnect" href="https://fonts.googleapis.com"/>
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
-          <link href="https://fonts.googleapis.com/css2?family=Karla:ital,wght@0,200..800;1,200..800&family=Markazi+Text:wght@400..700&display=swap" rel="stylesheet"/>
-        </Helmet>
-        <Nav/>
-        <Routes>
-          <Route path='/' element={<Homepage/>}/>
-          <Route path='/menu' element={<OnlineMenu/>}/>
-          <Route path='reservations' element={<Reservations/>}/>
-          <Route path='/confirmed' element={<ConfirmedBooking/>}/>
-          <Route path='/purchase' element={<Purchase/>}/>
-          <Route path='/delivery' element={<ConfirmedDelivery/>}/>
-        </Routes>
-        <Footer/>
-      </section>
-    </Router>
+    <section className='App'>
+      <Helmet>
+        <meta name="og:description" content="Little Lemon is a family-owned restaurant specialized in Mediterranean food with a modern twist"/>
+        <meta name="og:title" content='Little Lemon'/>
+        <meta name='og:image' content={Logo}/>
+        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+        <link href="https://fonts.googleapis.com/css2?family=Karla:ital,wght@0,200..800;1,200..800&family=Markazi+Text:wght@400..700&display=swap" rel="stylesheet"/>
+      </Helmet>
+      <Nav/>
+      <Routes>
+        <Route path='/' element={<Homepage/>}/>
+        <Route path='/menu' element={<OnlineMenu/>}/>
+        <Route path='reservations' element={<Reservations/>}/>
+        <Route path='/confirmed' element={<ConfirmedBooking/>}/>
+        <Route path='/purchase' element={<Purchase/>}/>
+        <Route path='/delivery' element={<ConfirmedDelivery/>}/>
+      </Routes>
+      <Footer/>
+    </section>
   );
 }
 
